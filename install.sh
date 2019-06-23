3fa5dc5145e50cc52c70c4e8ffa8530d758254f5
Sascha Müllner <sascha@muellner.de>
2019-06-23 16:38:05 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
  #!/bin/sh
 
 ###
-# pip && powerline-shell
+# pip
+easy_install=$(which easy_install)
 pip3=$(which pip3)
 pip=$(which pip)
 
 if [[ -x "${pip3}" ]]; then
 	$pip3 install --upgrade pip
-	$pip3 install powerline-shell
-elif [[ -x "${pip}" ]]; then
-	$pip install --upgrade pip
-	$pip install powerline-shell
+else
+	$easy_install pip
 fi
- 
+
+pip install --upgrade pip
+	
+###
+# powerline-shell
+powerline_shell=$(which powerline-shell)
+if [[ -x "${powerline_shell}" ]]; then
+    echo "✅ ${powerline_shell} already installed"
+else
+    echo "⚙️ Installing powerline-shell"
+	pip install powerline-shell
+fi
+	
+
 ###
 # bash
 bashrc="${HOME}/.bashrc"
@@ -22,7 +34,7 @@ if test -f "${bashrc}"; then
     if grep -q "${env_bashrc}" "${bashrc}"; then
     	echo "✅ ${env_bashrc} already sourced"
     else
-    	echo "✅ Sourcing ${env_bashrc}"
+    	echo "⚙️ Sourcing ${env_bashrc}"
     	echo "" >> ${bashrc}
     	echo "# Including env_bashrc" >> ${bashrc}
     	echo "test -e \"${env_bashrc}\" && source \"${env_bashrc}\"" >> ${bashrc}
@@ -40,7 +52,7 @@ if test -f "${zshrc}"; then
     if grep -q "${env_zshrrc}" "${zshrc}"; then
     	echo "✅ ${env_zshrrc} already sourced"
     else
-    	echo "✅ Sourcing ${env_zshrrc}"
+    	echo "⚙️ Sourcing ${env_zshrrc}"
     	echo "" >> ${zshrc}
     	echo "# Including env zshrc" >> ${zshrc}
     	echo "test -e \"${env_zshrrc}\" && source \"${env_zshrrc}\"" >> ${zshrc}
@@ -50,4 +62,6 @@ else
 fi
 
 
+###
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
